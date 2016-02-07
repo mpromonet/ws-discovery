58eebd20c4d8179e76be6026fd6ad042826f93fb
mpromonet <michel.promonet@free.fr>
2016-02-08 00:34:59 +0100

    fix proxy to collect several probematches

--- gsoap/proxy.cpp
+++ gsoap/proxy.cpp
@@ -20,7 +20,7 @@ void sighandler(int sig)
 	std::cout<< "Signal caught..." << std::endl;
 	stop = true;
 }
-	
+
 int main(int argc, char** argv)
 {
 	struct soap* tcpServer = soap_new(); 
@@ -45,7 +45,6 @@ int main(int argc, char** argv)
 	while (!stop)
 	{
 		soap_wsdd_listen(tcpServer, -1000000);
-		soap_wsdd_listen(udpServer, -1000000);
 	}
 
 	soap_destroy(udpServer);	
@@ -57,18 +56,36 @@ int main(int argc, char** argv)
 	return 0;
 }
 
+struct wsdd__ProbeMatchesType* outmatches;
+
 void wsdd_event_ProbeMatches(struct soap *soap, unsigned int InstanceId, const char *SequenceId, unsigned int MessageNumber, const char *MessageID, const char *RelatesTo, struct wsdd__ProbeMatchesType *matches)
 {
 	printf("wsdd_event_ProbeMatches\tid=%s RelatesTo=%s nbMatch:%d\n", MessageID, RelatesTo, matches->__sizeProbeMatch);
-	struct soap* serv = (struct soap*)soap->user;
-	soap_wsdd_ProbeMatches(serv, NULL, MessageID, RelatesTo, NULL, matches);	
+	struct soap* tcpServ = (struct soap*)soap->user;
+	if (outmatches != NULL)
+	{
+		for (int i=0; i < matches->__sizeProbeMatch; ++i)
+		{
+			wsdd__ProbeMatchType& match = matches->ProbeMatch[i];
+			char* address = (char*)soap_malloc(tcpServ, strlen(match.wsa__EndpointReference.Address));
+			strcpy(address, match.wsa__EndpointReference.Address);
+			char* types = (char*)soap_malloc(tcpServ, strlen(match.Types));
+			strcpy(types, match.Types);
+			char* scopes = (char*)soap_malloc(tcpServ, strlen(match.Scopes->__item));
+			strcpy(scopes, match.Scopes->__item);
+			char* XAddrs = (char*)soap_malloc(tcpServ, strlen(match.XAddrs));
+			strcpy(XAddrs, match.XAddrs);
+			printf("%s %s %s %s\n", address, types, scopes, XAddrs);
+			soap_wsdd_add_ProbeMatch(tcpServ, outmatches, address, types, scopes, NULL, XAddrs, match.MetadataVersion);
+		}	
+	}
 }
 
 void wsdd_event_ResolveMatches(struct soap *soap, unsigned int InstanceId, const char *SequenceId, unsigned int MessageNumber, const char *MessageID, const char *RelatesTo, struct wsdd__ResolveMatchType *match)
 {
 	printf("wsdd_event_ResolveMatches\tid=%s RelatesTo=%s\n", MessageID, RelatesTo);
-	struct soap* serv = (struct soap*)soap->user;
-	soap_wsdd_ResolveMatches(serv, NULL, MessageID, RelatesTo, NULL, match->wsa__EndpointReference.Address, match->Types, match->Scopes->__item, NULL, match->XAddrs, match->MetadataVersion);
+	struct soap* tcpServ = (struct soap*)soap->user;
+	soap_wsdd_ResolveMatches(tcpServ, NULL, MessageID, RelatesTo, NULL, match->wsa__EndpointReference.Address, match->Types, match->Scopes->__item, NULL, match->XAddrs, match->MetadataVersion);
 }
 
 void wsdd_event_Hello(struct soap *soap, unsigned int InstanceId, const char *SequenceId, unsigned int MessageNumber, const char *MessageID, const char *RelatesTo, const char *EndpointReference, const char *Types, const char *Scopes, const char *MatchBy, const char *XAddrs, unsigned int MetadataVersion)
@@ -84,23 +101,29 @@ void wsdd_event_Bye(struct soap *soap, unsigned int InstanceId, const char *Sequ
 soap_wsdd_mode wsdd_event_Resolve(struct soap *soap, const char *MessageID, const char *ReplyTo, const char *EndpointReference, struct wsdd__ResolveMatchType *match)
 {
 	printf("wsdd_event_Resolve\tid=%s replyTo=%s EndpointReference=%s\n", MessageID, ReplyTo, EndpointReference);	
-	struct soap* serv = (struct soap*)soap->user;
-	soap_wsdd_Resolve(serv,
-			  SOAP_WSDD_MANAGED,      // mode
+	
+	struct soap* udpServer = (struct soap*)soap->user;
+	soap_wsdd_Resolve(udpServer,
+			  SOAP_WSDD_ADHOC,      // mode
 			  SOAP_WSDD_TO_TS,      // to a TS
 			  "soap.udp://239.255.255.250:3702",         // address of TS
 			  MessageID,                   // message ID
 			  NULL,                 // ReplyTo
 			  EndpointReference);
-	return SOAP_WSDD_MANAGED;
+	soap_wsdd_listen(udpServer, -1000000);
+	
+	return SOAP_WSDD_ADHOC;
 }
 
 soap_wsdd_mode wsdd_event_Probe(struct soap *soap, const char *MessageID, const char *ReplyTo, const char *Types, const char *Scopes, const char *MatchBy, struct wsdd__ProbeMatchesType *matches)
 {
 	printf("wsdd_event_Probe\tid=%s replyTo=%s types=%s scopes=%s\n", MessageID, ReplyTo, Types, Scopes);	
-	struct soap* serv = (struct soap*)soap->user;
-	soap_wsdd_Probe(serv,
-			  SOAP_WSDD_MANAGED,      // mode
+	soap_wsdd_init_ProbeMatches(soap,matches);
+	
+	struct soap* udpServer = (struct soap*)soap->user;
+	outmatches = matches;
+	soap_wsdd_Probe(udpServer,
+			  SOAP_WSDD_ADHOC,      // mode
 			  SOAP_WSDD_TO_TS,      // to a TS
 			  "soap.udp://239.255.255.250:3702",         // address of TS
 			  MessageID,                   // message ID
@@ -108,6 +131,11 @@ soap_wsdd_mode wsdd_event_Probe(struct soap *soap, const char *MessageID, const 
 			  Types,
 			  Scopes,
 			  NULL);	
-	return SOAP_WSDD_MANAGED;
+	soap_wsdd_listen(udpServer, -1000000);
+	outmatches = NULL;
+	
+	soap_wsdd_ProbeMatches(soap, NULL, soap_wsa_rand_uuid(soap) , MessageID, ReplyTo, matches);
+	
+	return SOAP_WSDD_ADHOC;
 }
 
